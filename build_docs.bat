a8f08ddd75b38b9e8357f583ade1ed418f07df22
Michael Beyer <mgbeyer@gmx.de>
2018-05-26 17:37:38 +0200

    batch for phpDocumentor

--- build_docs.bat
+++ build_docs.bat
@@ -1,1 +1,2 @@
-..\phpDocumentor2\phpDocumentor-2.9.0\bin\phpdoc -d ./ -t ./docs/api --ignore dbal-2.5.4/*,examples/*
+D:\xampp\htdocs\phpDocumentor\bin\phpdoc -d ./ -t ./docs/api --ignore dbal-2.5.4/*,examples/*
+REM don't forget to set "PHP_PEAR_BIN_DIR" environment var to D:\xampp\htdocs\phpDocumentor\bin for "phpdoc.bat" to work properly
